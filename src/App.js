cded0386b57e71d8e56927653d200cf17d8af467
uriya1346 <95591955+uriya1346@users.noreply.github.com>
2022-05-29 12:04:31 +0300

    first-commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import "./app.css";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 32.075874,
+    longitude: 34.800098,
+    zoom: 12.7,
+  });
+  const [ar, setAr] = useState([]);
+  const [selectedParking, setSelectedParking] = useState(null);
+
+  useEffect(() => {
+    doApi()
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedParking(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  const doApi = async () => {
+    let url = "https://api.tel-aviv.gov.il/parking/stations";
+    let resp = await axios.get(url);
+    setAr(resp.data)
+  }
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoiZ3VyZ2VuOCIsImEiOiJjbDAya2p3YWQwNGI3M2txcHQ4amF0NW1tIn0.LifP-AAkjEyrty0gLCW53w"
+        width="100%"
+        height="100%"
+        transitionDuration="200"
+        mapStyle="mapbox://styles/gurgen8/cl02lbqvb000p14lakcg8t0s6"
+        onViewportChange={(viewport) => setViewport(viewport)}
+      >
+        {ar.map((item) => {
+          return (
+            <Marker
+              key={item.AhuzotCode}
+              latitude={Number(item.GPSLattitude)}
+              longitude={Number(item.GPSLongitude)}
+            >
+              <button
+                className="marker-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedParking(item);
+                }}
+              >
+                <img src="/marker.png" alt="Parking Icon" />
+              </button>
+            </Marker>
+          );
+        })}
+        {selectedParking ? (
+          <Popup
+            latitude={Number(selectedParking.GPSLattitude)}
+            longitude={Number(selectedParking.GPSLongitude)}
+            onClose={() => {
+              setSelectedParking(null);
+            }}
+          >
+            <div className="info text-center p-2" style={{width:"50vh"}}>
+              <h1>{selectedParking.Name}</h1>
+              <h3>{selectedParking.Address}</h3>
+              <h5>{selectedParking.FeeComments}</h5>
+              <h5>{selectedParking.DaytimeFee}</h5>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;
